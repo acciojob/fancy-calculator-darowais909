66cd08769a6a0fc3aed59f2a4aee034fba6923c4
Owayas Ahmad Dar <144412066+darowais909@users.noreply.github.com>
2023-12-18 23:02:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-//your JS code here. If required.
+var firstInput = prompt();
+var multipliedResult = parseFloat(firstInput) * 2;
+var secondInput = prompt();
+var finalResult = multipliedResult + parseFloat(secondInput);
+alert("The final result is: " + finalResult);
